a9bb820290165fd3acb4bd2a4c44b21516b4f5c1
Dongkyu Kim <stleamist@me.com>
2020-04-29 11:49:01 +0900

    UIColor의 init(rgb:alpha:) 메소드를 init(hex:)로 변경

--- Sources/ExtensionKit/UIKit/UIColor+Hex.swift
+++ Sources/ExtensionKit/UIKit/UIColor+Hex.swift
@@ -1,21 +1,17 @@
 import UIKit
 
 public extension UIColor {
-    convenience init(red: Int, green: Int, blue: Int, alpha: CGFloat = 1.0) {
+    
+    convenience init(hex: Int) {
+        let red = (hex >> 16) & 0xFF
+        let green = (hex >> 8) & 0xFF
+        let blue = hex & 0xFF
+        
         self.init(
             red: CGFloat(red) / 255.0,
             green: CGFloat(green) / 255.0,
             blue: CGFloat(blue) / 255.0,
-            alpha: alpha
-        )
-    }
-    
-    convenience init(rgb: Int, a: CGFloat = 1.0) {
-        self.init(
-            red: (rgb >> 16) & 0xFF,
-            green: (rgb >> 8) & 0xFF,
-            blue: rgb & 0xFF,
-            alpha: a
+            alpha: 1.0
         )
     }
 }
